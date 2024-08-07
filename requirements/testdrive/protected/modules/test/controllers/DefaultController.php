d19e7b21a314b7ba92984d8c80c0acae4e43a4a6
DxinCom <2573501810@qq.com>
2024-08-07 16:02:07 +0800

    代码注释

--- requirements/testdrive/protected/modules/test/controllers/DefaultController.php
+++ requirements/testdrive/protected/modules/test/controllers/DefaultController.php
@@ -4,9 +4,6 @@ class DefaultController extends Controller
 {
 	public function actionIndex()
 	{
-        echo 1;
-        echo "<pre>";print_r(Yii::app()->controller);exit;
-        echo "<pre>";print_r(Yii::app()->controller);exit;
 		$this->render('index');
 	}
 }
